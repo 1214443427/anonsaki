a7449b7945166210699cde1102a48f9d12142788
John Doe <you@example.com>
2026-01-12 21:26:10 -0800

    added import to popupmodal

--- src/pages/DatePage.jsx
+++ src/pages/DatePage.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PopUpModal from '../components/PopupModal'
 
 function DatePage() {
-    const doNotRemind = useState(()=>localStorage.getItem("doNotRemind")||false)
-    const [showModal, setShowModal] = useState(true)
-    return (
-        <div>
-            Date
-            <PopUpModal showModal={showModal} closeModal={()=>setShowModal(false)}>
-                <div>PWAhint</div>
-            </PopUpModal>
-        </div>
-    )
+  const doNotRemind = useState(()=>localStorage.getItem("doNotRemind")||false)
+  const isStandalone = window.matchMedia("(display-mode: standalone)").matches || navigator.standalone === true
+  const isMobile = /android|iphone|ipad|ipod/i.test(navigator.userAgent)
+  const [showModal, setShowModal] = useState(true)
+  const [deferredPrompt, setDeferredPrompt] = useState(null)
+  useEffect(()=>{
+    const handler = (e) => {
+      setDeferredPrompt(e)
+    }
+  })
+
+  return (
+    <div>
+        Date
+        <PopUpModal showModal={showModal} closeModal={()=>setShowModal(false)}>
+            <div>PWAhint</div>
+        </PopUpModal>
+    </div>
+  )
 }
 
 export default DatePage
